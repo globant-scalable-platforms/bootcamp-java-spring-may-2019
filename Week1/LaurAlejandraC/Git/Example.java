fa18fce5ffa20b1458bf2ee40d91d2560fbd0ade
Laura Alejandra Chaparro Gutiérrez <lauralejandracg@gmail.com>
2019-05-29 18:39:55 -0500

    Add substraction calculation to example

--- Week1/LaurAlejandraC/Git/Example.java
+++ Week1/LaurAlejandraC/Git/Example.java
@@ -5,11 +5,14 @@ class Example{
         int[] numbers = new int[5];
         Arrays.fill(numbers, 5);
         int sum = 0;
+        int substraction = 0;
 
         for(int i = 0; i < numbers.length; i++){
             sum += numbers[i];
+            substraction -= numbers[i];
         }
 
-        System.out.println(sum);
+        System.out.println("Sum: " + sum);
+        System.out.println("Substraction:" + substraction);
     }
 }
