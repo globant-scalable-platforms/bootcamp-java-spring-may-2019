15a362ed494d38601188eab97534af6ed48dabe5
Laura Alejandra Chaparro Gutiérrez <lauralejandracg@gmail.com>
2019-05-29 18:37:26 -0500

    Add product calculation to example

--- Week1/LaurAlejandraC/Git/Example.java
+++ Week1/LaurAlejandraC/Git/Example.java
@@ -5,11 +5,14 @@ class Example{
         int[] numbers = new int[5];
         Arrays.fill(numbers, 5);
         int sum = 0;
+        int product = 1;
 
         for(int i = 0; i < numbers.length; i++){
             sum += numbers[i];
+            product *= numbers[i];
         }
 
-        System.out.println(sum);
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
     }
 }
