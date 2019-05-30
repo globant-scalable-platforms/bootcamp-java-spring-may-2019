f48366c5a5cab4b0998ac24f917a6ac1d3e73d0a
Jonnathan Sanchez <jothsanchez25@gmail.com>
2019-05-29 23:44:56 -0500

    Changing message in other branch

--- Week1/JonnathanSanchez/Kata_Github/HelloWorld.java
+++ Week1/JonnathanSanchez/Kata_Github/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-    //First commit
+    //changing message in other branch
     public static void main(String[] args) {
-        System.out.println("Hello World!!! ");
+        System.out.println("Hola Mundo!!! ");
     }
 }
