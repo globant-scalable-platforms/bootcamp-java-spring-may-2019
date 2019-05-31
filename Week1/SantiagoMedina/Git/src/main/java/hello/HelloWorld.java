f51c1a48d2adb6887026dda24bcb564a821e1b50
santiago <santiagomed93@gmail.com>
2019-05-30 22:24:23 -0500

    Se modifica archivo main para que genere error

--- Week1/SantiagoMedina/Git/src/main/java/hello/HelloWorld.java
+++ Week1/SantiagoMedina/Git/src/main/java/hello/HelloWorld.java
@@ -3,8 +3,8 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
+  public  void main(String[] args) {
+    
     System.out.println("The current local time is: " + currentTime);
 
     Greeter greeter = new Greeter();
