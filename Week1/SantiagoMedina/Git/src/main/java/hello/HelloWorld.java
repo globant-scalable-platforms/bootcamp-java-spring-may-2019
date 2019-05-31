f4cea74463c0f70789c4814fd3f2504fb786c5c5
santiago <santiagomed93@gmail.com>
2019-05-30 22:30:12 -0500

    Se arreglan archivos

--- Week1/SantiagoMedina/Git/src/main/java/hello/HelloWorld.java
+++ Week1/SantiagoMedina/Git/src/main/java/hello/HelloWorld.java
@@ -3,8 +3,8 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-  public  void main(String[] args) {
-    LocalTime currentTime2 = new LocalTime();
+  public  static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
 
     Greeter greeter = new Greeter();
