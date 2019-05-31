8e72d4bd8a8dcf1fe4763f20cc07b952f81cb069
Santiago Collazos Barrera <santicollazosbarrera@hotmail.com>
2019-05-30 23:38:33 -0500

    Challenges Gradle
    Created in separated folders all the challenges.
    
    -In the first folder the test worked using shell
    
    -In the second folder the notacion to build the jar is inside app folder in the build.gradle file, and the jar is in the challenges' root folder
    
    -In the third folder  is the spring boot project, generate the jar and use shell to run the applitacion and it was executed in port 8080
    
    -In the fourth folder is the same spring boot project adding sonar plugin

---
+++ Week1/SantiagoCollazosBarrera/Gradle/Challenge 1/service/src/main/java/com/globant/bootcamp/gradle/service/ServiceMessage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.globant.bootcamp.gradle.service;
+
+/**
+ *
+ * @author SantiagoCollazosBarrera
+ */
+public class ServiceMessage {
+    public String getMessage(){
+        return "Hello Globant! from ServiceMessage; other project";
+    }
+}
