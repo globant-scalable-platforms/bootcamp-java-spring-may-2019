eddb59fdffa09e8f63ca83047e655f7bd173f37c
ingjuanmolina <50556513+ingjuanmolina@users.noreply.github.com>
2019-06-01 15:40:59 -0500

    Spring Starter Challenge
    Generate a gradle project with https://start.spring.io/ with a spring-web
    dependency.

---
+++ springwebdependency/src/main/java/com/webchallenge/springwebdependency/SpringwebdependencyApplication.java
@@ -0,0 +1,13 @@
+package com.webchallenge.springwebdependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebdependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebdependencyApplication.class, args);
+	}
+
+}
