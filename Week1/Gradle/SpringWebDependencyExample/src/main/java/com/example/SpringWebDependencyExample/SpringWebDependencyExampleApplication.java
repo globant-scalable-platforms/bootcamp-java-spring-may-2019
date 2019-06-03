a38e72186399eea763f9c33090804839b044e715
diesazul96 <diesazul96@hotmail.com>
2019-06-03 13:14:46 -0500

    Spring Web Gradle Project

---
+++ Week1/Gradle/SpringWebDependencyExample/src/main/java/com/example/SpringWebDependencyExample/SpringWebDependencyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebDependencyExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDependencyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDependencyExampleApplication.class, args);
+	}
+
+}
