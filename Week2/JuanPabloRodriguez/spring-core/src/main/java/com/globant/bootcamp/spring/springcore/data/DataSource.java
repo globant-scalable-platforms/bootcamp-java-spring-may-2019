fe626a9d11b3e2a8b4abc2dd6aaa21a99367602b
juanpablorp96 <32114675+juanpablorp96@users.noreply.github.com>
2019-06-07 12:52:46 -0500

    Change List<Account> to HasMap<String, String>

--- Week2/JuanPabloRodriguez/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/data/DataSource.java
+++ Week2/JuanPabloRodriguez/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/data/DataSource.java
@@ -1,40 +1,39 @@
 package com.globant.bootcamp.spring.springcore.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.globant.bootcamp.spring.springcore.domain.Account;
 
 public class DataSource {
 
-	private static List<Account> accounts;
+	//private static List<Account> accounts;
+	private static Map<String, String> accounts;
 
 	public DataSource() {
 
-		accounts = new ArrayList<Account>();
-
-		accounts.add(new Account("Ana", "pswana"));
-		accounts.add(new Account("Juan", "pswjuan"));
-		accounts.add(new Account("Pablo", "pswpablo"));
-		accounts.add(new Account("Maria", "pswmaria"));
-		accounts.add(new Account("Camilo", "pswcamilo"));
-		accounts.add(new Account("Mike", "pswmike"));
-		accounts.add(new Account("Jhon", "pswjhon"));
-		accounts.add(new Account("Andy", "pswandy"));
-		accounts.add(new Account("Carl", "pswcarl"));
-		accounts.add(new Account("Lina", "pswlina"));
+		//accounts = new ArrayList<Account>();
+		accounts = new HashMap<>();
+		
+		accounts.put("Ana", "pswana");
+		accounts.put("Juan", "pswjuan");
+		accounts.put("Pablo", "pswpablo");
+		accounts.put("Maria", "pswmaria");
+		accounts.put("Camilo", "pswcamilo");
 
 	}
 
 	public Account retrieveAccountInformation(String username) {
 
-		Account result = accounts.stream()
-				.filter(x -> username.equals(x.getUsername()))
-				.findAny()
-				.orElse(null);
-
-		return result;
-
+		if(accounts.containsKey(username)) {
+			return new Account(username, accounts.get(username));
+		}
+		else {
+			return null;
+		}
+		
 	}
 
 }
