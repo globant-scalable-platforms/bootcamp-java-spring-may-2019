3fc739a0583b4a09493c1812ad46a819c3fdd546
juanpablorp96 <32114675+juanpablorp96@users.noreply.github.com>
2019-06-08 15:22:13 -0500

    Kata #2

--- Week2/JuanPabloRodriguez/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/configuration/BeansConfiguration.java
+++ Week2/JuanPabloRodriguez/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/configuration/BeansConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import com.globant.bootcamp.spring.springcore.data.DataSource;
+import com.globant.bootcamp.spring.springcore.domain.Game;
 import com.globant.bootcamp.spring.springcore.domain.Score;
 
 @Configuration
@@ -20,6 +21,24 @@ public class BeansConfiguration {
     @Scope("prototype")
     public Score scorePrototype() {
         return new Score(0, "I'm a prototype bean");
+    }
+    
+    @Bean
+    @Scope("singleton")
+    public Game gameSingleton1() {
+        return new Game("Player 1 (singleton) ->", 0, 0, false, "playing");
+    }
+    
+    @Bean
+    @Scope("singleton")
+    public Game gameSingleton2() {
+        return new Game("Player 2 (singleton) ->", 0, 0, false, "playing");
+    }
+    
+    @Bean
+    @Scope("prototype")
+    public Game gamePrototype() {
+        return new Game("Random number (prototype) ->", 0, 0, false, "playing");
     }
     
     @Bean
