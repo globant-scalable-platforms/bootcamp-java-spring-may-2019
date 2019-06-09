770d957a441715e2baf1ddc87ed480742dfad0ec
Santiago Vasquez <danielvas17@gmail.com>
2019-06-09 10:21:16 -0500

    Rest Challenge project init

---
+++ Week2/SantiagoVasquez/RestChallenge/restchallenge/src/main/java/com/bootcamp/restchallenge/RestchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.restchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestchallengeApplication.class, args);
+    }
+
+}
