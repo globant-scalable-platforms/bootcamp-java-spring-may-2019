431352ed8e5f65e0fd26efb6d00e97731b504c63
ledavid0520 <ledavis275@gmail.com>
2019-06-09 00:10:08 -0500

    COMMIT: Kata 2 Game

--- Week2/DavidAcuna/src/main/java/davidAcuna/configuration/BeansConfiguration.java
+++ Week2/DavidAcuna/src/main/java/davidAcuna/configuration/BeansConfiguration.java
@@ -1,9 +1,12 @@
 package davidAcuna.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import davidAcuna.domain.Game;
+import davidAcuna.domain.Player;
 import davidAcuna.domain.Score;
 
 @Configuration
@@ -19,6 +22,21 @@ public class BeansConfiguration {
     @Scope("prototype")
     public Score scorePrototype() {
         return new Score(0, "I'm a prototype bean");
+    }
+    
+    //    Game Configuration
+    @Bean
+    @Scope("prototype")
+    @Autowired
+    public Player playerScope() {
+        return new Player();
+    }
+    
+    @Bean
+    @Scope("singleton")
+    @Autowired 
+    public Game gameSingleton() {
+        return new Game();
     }
 
 }
