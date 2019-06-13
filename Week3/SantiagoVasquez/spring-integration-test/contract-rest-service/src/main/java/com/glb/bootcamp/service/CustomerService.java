924c6965e8cdb5933dc33f1494757ec9f70760ce
Santiago Vasquez <danielvas17@gmail.com>
2019-06-13 18:27:43 -0500

    Kata #1 project init

---
+++ Week3/SantiagoVasquez/spring-integration-test/contract-rest-service/src/main/java/com/glb/bootcamp/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.glb.bootcamp.service;
+
+import com.glb.bootcamp.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CustomerService {
+
+    private final Map<Long, Customer> customers;
+
+    public CustomerService() {
+        customers = new HashMap<>();
+        customers.put(1L, new Customer(1L, "Mike", "Castro"));
+        customers.put(2L, new Customer(2L, "Mauro", "Perez"));
+        customers.put(3L, new Customer(3L, "Daniel", "Pelaez"));
+    }
+
+    public Customer findCustomerById(Long id) {
+        return customers.get(id);
+    }
+}
