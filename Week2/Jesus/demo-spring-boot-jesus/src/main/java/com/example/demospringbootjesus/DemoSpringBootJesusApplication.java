07bad48deb1b2c5ac79f4df0fe1c04b0418da12e
Jesus <jesus4j@gmail.com>
2019-06-14 13:34:27 -0500

    se actualiza trabajo

---
+++ Week2/Jesus/demo-spring-boot-jesus/src/main/java/com/example/demospringbootjesus/DemoSpringBootJesusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootjesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootJesusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJesusApplication.class, args);
+	}
+
+}
