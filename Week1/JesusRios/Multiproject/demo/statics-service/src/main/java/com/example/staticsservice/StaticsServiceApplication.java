99c6cd5bcf7b1542fc2de5fc240f1e3dc968d99a
Jesus <jesus4j@gmail.com>
2019-06-17 13:28:25 -0500

    se actualizan challenges

---
+++ Week1/JesusRios/Multiproject/demo/statics-service/src/main/java/com/example/staticsservice/StaticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.staticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticsServiceApplication.class, args);
+	}
+
+}
