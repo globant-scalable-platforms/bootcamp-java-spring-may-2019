70c92d08dec18f5eac2ac5d33d8f4e2a5e698587
santiago <santiagomed93@gmail.com>
2019-06-17 16:03:00 -0500

    Create Swagger project

---
+++ Week3/SantiagoMedina/swagger/src/main/java/com/glb/bootcamp/service/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.glb.bootcamp.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.glb.bootcamp.model.Customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+	private final Map<Long, Customer> customers;
+	private Long idCounter = 0L;
+	
+	public CustomerServiceImpl() {
+		customers = new HashMap<>();
+		customers.put(1L, new Customer(1L, "Mike", "Castro"));
+		customers.put(2L, new Customer(2L, "Mauro", "Perez"));
+		customers.put(3L, new Customer(3L, "Daniel", "Pelaez"));
+		this.idCounter = customers.size() + 0L;
+	}
+	
+	public Map<Long, Customer> getAll(){
+		return customers;
+	}
+	
+	public Customer findCustomerById(Long id) {
+		return customers.get(id);
+	}
+	
+	public Customer createCustomer(String name, String surname) {
+		idCounter = idCounter + 1L;
+		customers.put(idCounter, new Customer(idCounter, name, surname));
+		return customers.get(idCounter);
+	}
+	
+	public void deleteCustomer(Long id) {
+		customers.remove(id);
+	}
+	
+	public void updateCustomer(Long id, String name, String surname) {
+		customers.get(id).setName(name);
+		customers.get(id).setSurname(surname);
+	}
+}
